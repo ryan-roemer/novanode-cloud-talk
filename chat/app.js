1328da8543e3fe942cc11e8f6be56110f93d537e
Ryan Roemer <ryan@loose-bits.com>
2012-03-17 16:20:26 -0400

    Add demo from https://github.com/steffenwt/nodejs-pub-sub-chat-demo .

---
+++ chat/app.js
@@ -0,0 +1,34 @@
+var app = require('http').createServer(handler),
+	io = require('socket.io').listen(app),
+	fs = require('fs');
+	
+	app.listen(80); //, "192.168.178.34");
+	
+	function handler(req, res) { // just return index.html
+		fs.readFile(__dirname + '/index.html',
+			function (err, data) {
+				if (err) {
+					res.writeHead(500);
+					return res.end('Error loading index.html');
+				}
+				
+				res.writeHead(200);
+				res.end(data);
+			});
+	}
+
+	io.sockets.on('connection', function (socket) { // handler for incoming connections
+		socket.on('chat', function (data) {
+			var msg = JSON.parse(data);
+			var reply = JSON.stringify({action: 'message', user: msg.user, msg: msg.msg });
+			socket.emit('chat', reply);
+			socket.broadcast.emit('chat', reply);
+		});
+
+		socket.on('join', function(data) {
+			var msg = JSON.parse(data);
+			var reply = JSON.stringify({action: 'control', user: msg.user, msg: ' joined the channel' });
+			socket.emit('chat', reply);
+			socket.broadcast.emit('chat', reply);
+		});
+	});
