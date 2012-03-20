ecf7654a858876d959301e933326d4316982ac3a
Ryan Roemer <ryan@loose-bits.com>
2012-03-20 15:08:37 -0400

    Update hello2.js.

--- server-hello2.js
+++ server-hello2.js
@@ -13,15 +13,13 @@ http.createServer(function (req, res) {
     '    <link rel="stylesheet" href="http://twitter.github.com/bootstrap/1.4.0/bootstrap.min.css" />' +
     "  </head>" +
     "  <body>" +
-    '    <div class="container"><div class="hero-unit">' +
-    "      <h1>Hello World!</h1>" +
-    "      <p>" +
-    "         Your path is: \"<b>" + req.url + "</b>\"." +
-    "         You're calling from: <b>" + req.connection.remoteAddress + "</b>." +
-    "         Your browser identifies as: \"<b>" + req.headers["user-agent"] + "</b>\"." +
-    "         Your favorite color is: <span style='background: #" + randColor + "'>" +
-    "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>" +
-    "      </p>" +
+    '    <div class="container" style="width: 600px;"><div class="hero-unit">' +
+    "      <h1 style='text-align: center;'>Hello World!</h1><br />" +
+    "      <p>Your path is: \"<b>" + req.url + "</b>\"" +
+    "         and you're calling from: <b>" + req.connection.remoteAddress + "</b>.</p>" +
+    "      <p>Your browser identifies as: \"<b>" + req.headers["user-agent"].substr(0, 60) + " ...</b>\".</p>" +
+    "      <p>Your favorite color is: <span style='background: #" + randColor + "'>" +
+    "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></p>" +
     '    </div></div>' +
     "  </body>" +
     "</html>"
