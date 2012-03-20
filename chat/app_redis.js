f8abf8b334af4eb8315452892c2eeccf4c5b90ba
Ryan Roemer <ryan@loose-bits.com>
2012-03-20 15:42:21 -0400

    Chat slides. Convert tabs to spaces.

--- chat/app_redis.js
+++ chat/app_redis.js
@@ -1,7 +1,7 @@
 var app = require('http').createServer(handler),
-	io = require('socket.io').listen(app),
-	fs = require('fs'),
-	redis = require('redis'),
+  io = require('socket.io').listen(app),
+  fs = require('fs'),
+  redis = require('redis'),
   ADDR = process.env.ADDRESS || "0.0.0.0",
   PORT = process.env.PORT || 2000,
   redisUrl = require('url').parse(process.env.REDISTOGO_URL ||
@@ -15,7 +15,7 @@ app.listen(PORT, ADDR);
 console.log("Server running at http://%s:%s/", ADDR, PORT);
 
 function handler(req, res) {
-	// just return the index HTML
+  // just return the index HTML
   var pageFn = function (err, data) {
     if (err) {
       res.writeHead(500);
@@ -38,7 +38,7 @@ function handler(req, res) {
 }
 
 io.configure(function () {
-	io.set('close timeout', 60*60*24); // 24h time out
+  io.set('close timeout', 60*60*24); // 24h time out
 
   // Heroku doesn't support websockets, so let's tune long polling.
   io.set("polling duration", 2);
@@ -47,11 +47,11 @@ io.configure(function () {
 function SessionController (user) {
   var self = this;
 
-	// session controller class for storing redis connections
-	// this is more a workaround for the proof-of-concept
-	// in "real" applications session handling should NOT
-	// be done like this
-	self.sub = redis.createClient(redisPort, redisHost);
+  // session controller class for storing redis connections
+  // this is more a workaround for the proof-of-concept
+  // in "real" applications session handling should NOT
+  // be done like this
+  self.sub = redis.createClient(redisPort, redisHost);
   if (redisAuth) {
     self.sub.auth(redisAuth);
   }
@@ -61,83 +61,83 @@ function SessionController (user) {
     self.pub.auth(redisAuth);
   }
 
-	this.user = user;
+  this.user = user;
 }
 
 SessionController.prototype.subscribe = function(socket) {
-	this.sub.on('message', function(channel, message) {
-		socket.emit(channel, message);
-	});
-	var current = this;
-	this.sub.on('subscribe', function(channel, count) {
-		var joinMessage = JSON.stringify({action: 'control', user: current.user, msg: ' joined the channel' });
-		current.publish(joinMessage);
-	});
-	this.sub.subscribe('chat');
+  this.sub.on('message', function(channel, message) {
+    socket.emit(channel, message);
+  });
+  var current = this;
+  this.sub.on('subscribe', function(channel, count) {
+    var joinMessage = JSON.stringify({action: 'control', user: current.user, msg: ' joined the channel' });
+    current.publish(joinMessage);
+  });
+  this.sub.subscribe('chat');
 };
 
 SessionController.prototype.rejoin = function(socket, message) {
-	this.sub.on('message', function(channel, message) {
-		socket.emit(channel, message);
-	});
-	var current = this;
-	this.sub.on('subscribe', function(channel, count) {
-		var rejoin = JSON.stringify({action: 'control', user: current.user, msg: ' rejoined the channel' });
-		current.publish(rejoin);
-		var reply = JSON.stringify({action: 'message', user: message.user, msg: message.msg });
-		current.publish(reply);
-	});
-	this.sub.subscribe('chat');
+  this.sub.on('message', function(channel, message) {
+    socket.emit(channel, message);
+  });
+  var current = this;
+  this.sub.on('subscribe', function(channel, count) {
+    var rejoin = JSON.stringify({action: 'control', user: current.user, msg: ' rejoined the channel' });
+    current.publish(rejoin);
+    var reply = JSON.stringify({action: 'message', user: message.user, msg: message.msg });
+    current.publish(reply);
+  });
+  this.sub.subscribe('chat');
 };
 
 SessionController.prototype.unsubscribe = function() {
-	this.sub.unsubscribe('chat');
+  this.sub.unsubscribe('chat');
 };
 
 SessionController.prototype.publish = function(message) {
-	this.pub.publish('chat', message);
+  this.pub.publish('chat', message);
 };
 
 SessionController.prototype.destroyRedis = function() {
-	if (this.sub !== null) this.sub.quit();
-	if (this.pub !== null) this.pub.quit();
+  if (this.sub !== null) this.sub.quit();
+  if (this.pub !== null) this.pub.quit();
 };
 
 io.sockets.on('connection', function (socket) { // the actual socket callback
-	console.log(socket.id);
-	socket.on('chat', function (data) { // receiving chat messages
-		var msg = JSON.parse(data);
-		socket.get('sessionController', function(err, sessionController) {
-			if (sessionController === null) {
-				// implicit login - socket can be timed out or disconnected
-				var newSessionController = new SessionController(msg.user);
-				socket.set('sessionController', newSessionController);
-				newSessionController.rejoin(socket, msg);
-			} else {
-				var reply = JSON.stringify({action: 'message', user: msg.user, msg: msg.msg });
-				sessionController.publish(reply);
-			}
-		});
-		// just some logging to trace the chat data
-		console.log(data);
-	});
+  console.log(socket.id);
+  socket.on('chat', function (data) { // receiving chat messages
+    var msg = JSON.parse(data);
+    socket.get('sessionController', function(err, sessionController) {
+      if (sessionController === null) {
+        // implicit login - socket can be timed out or disconnected
+        var newSessionController = new SessionController(msg.user);
+        socket.set('sessionController', newSessionController);
+        newSessionController.rejoin(socket, msg);
+      } else {
+        var reply = JSON.stringify({action: 'message', user: msg.user, msg: msg.msg });
+        sessionController.publish(reply);
+      }
+    });
+    // just some logging to trace the chat data
+    console.log(data);
+  });
 
-	socket.on('join', function(data) {
-		var msg = JSON.parse(data);
-		var sessionController = new SessionController(msg.user);
-		socket.set('sessionController', sessionController);
-		sessionController.subscribe(socket);
-		// just some logging to trace the chat data
-		console.log(data);
-	});
+  socket.on('join', function(data) {
+    var msg = JSON.parse(data);
+    var sessionController = new SessionController(msg.user);
+    socket.set('sessionController', sessionController);
+    sessionController.subscribe(socket);
+    // just some logging to trace the chat data
+    console.log(data);
+  });
 
-	socket.on('disconnect', function() { // disconnect from a socket - might happen quite frequently depending on network quality
-		socket.get('sessionController', function(err, sessionController) {
-			if (sessionController === null) return;
-			sessionController.unsubscribe();
-			var leaveMessage = JSON.stringify({action: 'control', user: sessionController.user, msg: ' left the channel' });
-			sessionController.publish(leaveMessage);
-			sessionController.destroyRedis();
-		});
-	});
+  socket.on('disconnect', function() { // disconnect from a socket - might happen quite frequently depending on network quality
+    socket.get('sessionController', function(err, sessionController) {
+      if (sessionController === null) return;
+      sessionController.unsubscribe();
+      var leaveMessage = JSON.stringify({action: 'control', user: sessionController.user, msg: ' left the channel' });
+      sessionController.publish(leaveMessage);
+      sessionController.destroyRedis();
+    });
+  });
 });
